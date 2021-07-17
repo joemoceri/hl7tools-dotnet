494cd4e83b76a043c3fa1db6def5fc4901bb6f89
Joe Moceri <jmoceri34@gmail.com>
2021-07-17 11:05:25 -0700

    changes

--- ExpressionEvaluatorForDotNet.HL7V2VersionGenerator/App.cs
+++ ExpressionEvaluatorForDotNet.HL7V2VersionGenerator/App.cs
@@ -10,7 +10,7 @@ namespace ExpressionEvaluatorForDotNet.HL7V2VersionGenerator
             var caristixService = new CaristixService();
 
             // TODO: Pull data from caristix api, create folders, create .cs files for everything
-            var basePath = Directory.GetParent(Environment.NewLine).Parent.Parent.FullName;
+            var basePath = Directory.GetParent(Environment.NewLine).Parent.Parent.Parent.FullName;
             var versionsBasePath = Path.Combine(basePath, "Versions");
 
             Directory.CreateDirectory(versionsBasePath);
@@ -20,29 +20,8 @@ namespace ExpressionEvaluatorForDotNet.HL7V2VersionGenerator
             foreach (var version in versions)
             {
                 Directory.CreateDirectory(Path.Combine(versionsBasePath, version));
+                //var triggerEvents = caristixService.GetTriggerEvents(version);
             }
-
-            // v21
-
-            // v22
-
-            // v23
-
-            // v231
-
-            // v24
-
-            // v25
-
-            // v251
-
-            // v26
-
-            // v27
-
-            // v271
-
-            // v28
         }
     }
 }
---
+++ ExpressionEvaluatorForDotNet.HL7V2VersionGenerator/Caristix/CaristixService.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExpressionEvaluatorForDotNet.HL7V2VersionGenerator
+{
+    public class CaristixService
+    {
+        private readonly IRestClient restClient;
+        private readonly string baseUrl;
+
+        public CaristixService()
+        {
+            baseUrl = "https://hl7-definition.caristix.com/v2-api/1/HL7v";
+            restClient = new RestClient();
+        }
+
+        public IEnumerable<string> GetVersions()
+        {
+            return new List<string>
+            {
+                "2.1",
+                "2.2",
+                "2.3",
+                "2.3.1",
+                "2.4",
+                "2.5",
+                "2.5.1",
+                "2.6",
+                "2.7",
+                "2.7.1",
+                "2.8",
+            };
+        }
+
+        public JArray GetTriggerEvents(string version)
+        {
+            var request = new RestRequest($"{baseUrl}{version}/TriggerEvents", Method.GET);
+
+            var response = restClient.Execute<string>(request);
+
+            var result = JsonConvert.DeserializeObject<JArray>(response.Data.Trim('"'));
+
+            return result;
+        }
+    }
+}
