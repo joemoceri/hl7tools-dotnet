130b10622b0572f6fcb8385b0a5804887d873382
Joe Moceri <joseph.thomas.moceri@gmail.com>
2021-08-08 20:45:15 -0700

    changes

---
+++ src/ExpressionEvaluatorForDotNet.HL7V2VersionGenerator/HL7V2FieldData.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace ExpressionEvaluatorForDotNet
+{
+    public class HL7V2FieldData
+    {
+        public string Id { get; set; }
+
+        public string Type { get; set; }
+
+        public string Position { get; set; }
+
+        public string Name { get; set; }
+
+        public int Length { get; set; }
+
+        public string Usage { get; set; }
+
+        public string Rpt { get; set; }
+
+        public string DataType { get; set; }
+
+        public string DataTypeName { get; set; }
+
+        public string TableId { get; set; }
+
+        public string TableName { get; set; }
+
+        public string Description { get; set; }
+
+        public string Sample { get; set; }
+
+        public IList<HL7V2FieldData> FieldDatas { get; set; }
+    }
+}
--- src/ExpressionEvaluatorForDotNet.HL7V2VersionGenerator/Output/V22/TriggerEvents/HL7V22TriggerEventACK.cs
+++ src/ExpressionEvaluatorForDotNet.HL7V2VersionGenerator/Output/V22/TriggerEvents/HL7V22TriggerEventACK.cs
@@ -5,9 +5,9 @@ namespace ExpressionEvaluatorForDotNet
     public class HL7V22TriggerEventACK
     {
         public HL7V2Message message { get; init; }
-        public HL7V22SegmentMSH msh{ get; init; }
-public HL7V22SegmentMSA msa{ get; init; }
-public HL7V22SegmentERR err{ get; init; }
+        public HL7V22SegmentMSH msh { get; init; }
+        public HL7V22SegmentMSA msa { get; init; }
+        public HL7V22SegmentERR err { get; init; }
 
 
         public string Id { get { return @"ACK"; } }
@@ -16,9 +16,9 @@ public HL7V22SegmentERR err{ get; init; }
 
         public string Sample { get { return null; } }
 
-        public IList<string> Chapters 
-        { 
-            get 
+        public IList<string> Chapters
+        {
+            get
             {
                 return new[]
                     {
@@ -32,8 +32,8 @@ public HL7V22SegmentERR err{ get; init; }
         {
             this.message = message;
             this.msh = new HL7V22SegmentMSH(this.message);
-this.msa = new HL7V22SegmentMSA(this.message);
-this.err = new HL7V22SegmentERR(this.message);
+            this.msa = new HL7V22SegmentMSA(this.message);
+            this.err = new HL7V22SegmentERR(this.message);
 
         }
     }
